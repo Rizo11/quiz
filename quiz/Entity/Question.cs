b2a50696e2a11dfee72a2c9ca9a34b54d049a2e0
Rizo11 <maribjonov2002@gmail.com>
2022-04-07 13:07:56 +0500

    callback done

--- quiz/Entity/Question.cs
+++ quiz/Entity/Question.cs
@@ -6,10 +6,16 @@ public class Question
     public string QuestionText { get; set; }
     public Dictionary<string, bool> Answers { get; set; }
 
-    public Question(string questionText, Dictionary<string, bool> answers)
+    public Question(string questionText="What is your name", Dictionary<string, bool> answers = null)
     {
         QuestionText = questionText;
-        Answers = answers;
+        Answers = answers ?? new Dictionary<string, bool>()
+        {
+            { "Answer 1", true },
+            { "Answer 2", false },
+            { "Answer 3", false },
+            { "Answer 4", false }
+        };
     }
 }
 
